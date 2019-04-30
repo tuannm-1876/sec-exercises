1b6e64dcc61b1c7be55edb0a198f9a96fde211b1
Tuấn <nguy-minh-tuan@sun-asterisk.com>
2019-04-30 23:37:59 +0700

    tuannm-1876

---
+++ 0x01/solve/my_base64_decode.py
@@ -0,0 +1,4 @@
+import base64
+string = 'c3RyaW5nIGFiYw=='
+decoded = base64.b64decode(string)
+print decoded
---
+++ 0x01/solve/my_base64_encode.py
@@ -0,0 +1,4 @@
+import base64
+string = 'string abc'
+encoded = base64.b64encode(string)
+print encoded
